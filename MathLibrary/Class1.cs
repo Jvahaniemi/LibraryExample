31096f3f7f50eed661c26aff5f4b2853768ecb6e
unknown <k1201190@CA440411.epedu.local>
2015-02-05 17:50:46 +0200

    eka versio

---
+++ MathLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathLibrary
+{
+    public class AddClass
+    {
+        public static long Add(long i, long j)
+        {
+            return (i + j);
+        }
+    }
+
+    public class MultiplyClass
+    {
+        public static long Multiply(long x, long y)
+        {
+            return (x * y);
+        }
+    }
+}
